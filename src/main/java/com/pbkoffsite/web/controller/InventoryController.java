a4a8095abb560fce458349558bb24719f0cfdb1f
jmw <jmw5598@gmail.com>
2016-06-17 19:48:23 -0400

    changed depricated @AuthenticationPrincipal

--- src/main/java/com/pbkoffsite/web/controller/InventoryController.java
+++ src/main/java/com/pbkoffsite/web/controller/InventoryController.java
@@ -1,9 +1,7 @@
 package com.pbkoffsite.web.controller;
 
-import java.security.Principal;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
