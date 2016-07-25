6d37913b20c9e92b5fa033323d8b21b60a3b3a7f
jmw <jmw5598@gmail.com>
2016-07-25 10:53:28 -0400

    Added item count field

--- src/main/java/com/pbkoffsite/web/bean/entity/Stockroom.java
+++ src/main/java/com/pbkoffsite/web/bean/entity/Stockroom.java
@@ -2,17 +2,19 @@ package com.pbkoffsite.web.bean.entity;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
+import javax.persistence.CascadeType;		
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name="stockroom")
 public class Stockroom extends SimpleIdDescription {
 	
 	
-	//private int count;
+	@Transient
+	private long count;
 	
 	@OneToMany(mappedBy="stockroom", cascade={CascadeType.PERSIST})
 	private List<Item> items;
@@ -32,9 +34,20 @@ public class Stockroom extends SimpleIdDescription {
 		this.items.add(item);
 	}
 
+	public long getCount() {
+		return count;
+	}
+
+	public void setCount(long count) {
+		this.count = count;
+	}
+
 	@Override
 	public String toString() {
-		return "Stockroom [id=" + id + ", description=" + description + "]";
+		return "Stockroom [id=" + id + ", description=" + description + ", count="
+				+ count + "]";
 	}
+	
+	
 	
 }
