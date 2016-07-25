d226218f3961a99a68ff572ac8b0b2b861f9ce27
jmw <jmw5598@gmail.com>
2016-07-25 10:49:30 -0400

    Added new user details classes. Auth for logging in, and Basic for Item details.

---
+++ src/main/java/com/pbkoffsite/web/bean/entity/BasicUserDetails.java
@@ -0,0 +1,10 @@
+package com.pbkoffsite.web.bean.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class BasicUserDetails extends AbstractUser {
+	
+}
