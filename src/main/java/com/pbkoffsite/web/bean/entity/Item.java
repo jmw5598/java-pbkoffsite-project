ae8f30af2c5f641c70edf503c4e37a02c1b4fd23
jmw <jmw5598@gmail.com>
2016-07-26 11:01:50 -0400

    added Image entity.  Added Images to Item class

--- src/main/java/com/pbkoffsite/web/bean/entity/Item.java
+++ src/main/java/com/pbkoffsite/web/bean/entity/Item.java
@@ -1,15 +1,21 @@
 package com.pbkoffsite.web.bean.entity;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -31,12 +37,16 @@ public class Item {
 	@Column(name="date_added")
 	private Date dateAdded;
 
-	// link to added by user : Use BasicUserDetails class
+	@ManyToOne
+	@JoinColumn(name="added_by_id")
+	private BasicUserDetails addedBy;
 	
 	@Column(name="date_removed")
 	private Date dateRemoved;
 	
-	// link to removed by user : Use BasicUserDetails class
+	@ManyToOne
+	@JoinColumn(name="removed_by_id")
+	private BasicUserDetails removedBy;
 	
 	@ManyToOne(cascade={CascadeType.PERSIST})
 	@JoinColumn(name="removed_reason_id")
@@ -56,7 +66,16 @@ public class Item {
 	@JoinColumn(name="item_condition_id")
 	private ItemCondition itemCondition;
 	
+	@ManyToMany(cascade={CascadeType.PERSIST}, fetch=FetchType.EAGER)
+	@JoinTable(
+		name="item_image",
+		joinColumns={@JoinColumn(name="item_id")},
+		inverseJoinColumns={@JoinColumn(name="image_id")}
+	)
+	private List<Image> images;
+	
 	public Item() {}
+
 
 	public int getId() {
 		return id;
@@ -90,12 +109,28 @@ public class Item {
 		this.dateAdded = dateAdded;
 	}
 
+	public BasicUserDetails getAddedBy() {
+		return addedBy;
+	}
+
+	public void setAddedBy(BasicUserDetails addedBy) {
+		this.addedBy = addedBy;
+	}
+
 	public Date getDateRemoved() {
 		return dateRemoved;
 	}
 
 	public void setDateRemoved(Date dateRemoved) {
 		this.dateRemoved = dateRemoved;
+	}
+
+	public BasicUserDetails getRemovedBy() {
+		return removedBy;
+	}
+
+	public void setRemovedBy(BasicUserDetails removedBy) {
+		this.removedBy = removedBy;
 	}
 
 	public RemovedReason getRemovedReason() {
@@ -138,16 +173,30 @@ public class Item {
 		this.itemCondition = itemCondition;
 	}
 
+	public List<Image> getImages() {
+		return images;
+	}
+
+	public void setImages(List<Image> images) {
+		this.images = images;
+	}
+	
+	public void addImage(Image image) {
+		this.images.add(image);
+	}
+
+
 	@Override
 	public String toString() {
 		return "Item [id=" + id + ", location=" + location + ", sku=" + sku
-				+ ", dateAdded=" + dateAdded + ", dateRemoved=" + dateRemoved
+				+ ", dateAdded=" + dateAdded + ", addedBy=" + addedBy
+				+ ", dateRemoved=" + dateRemoved + ", removedBy=" + removedBy
 				+ ", removedReason=" + removedReason + ", additionNotes="
 				+ additionNotes + ", isAvailable=" + isAvailable
 				+ ", stockroom=" + stockroom + ", itemCondition="
-				+ itemCondition + "]";
+				+ itemCondition + ", images=" + images + "]";
 	}
-
-		
+	
+			
 	
 }
