f3d7117de2a4dd210b32274e3e9b19d90a6ef617
jmw <jmw5598@gmail.com>
2016-07-28 14:48:58 -0400

    added functionality to create a new user

--- src/main/java/com/pbkoffsite/web/repository/UserRepository.java
+++ src/main/java/com/pbkoffsite/web/repository/UserRepository.java
@@ -5,10 +5,15 @@ import java.util.List;
 
 import com.pbkoffsite.web.bean.AuthUserDetails;
 import com.pbkoffsite.web.bean.Role;
+import com.pbkoffsite.web.bean.UserForm;
 
 public interface UserRepository {
 	
+	List<AuthUserDetails> list();
+	List<Role> listRoles();
 	AuthUserDetails findUserByUsername(String username);
 	Collection<Role> findRolesByUsername(String username);
+	AuthUserDetails create(UserForm user);
+	AuthUserDetails update(AuthUserDetails user);
 	
 }
--- src/main/java/com/pbkoffsite/web/repository/UserRepositoryImpl.java
+++ src/main/java/com/pbkoffsite/web/repository/UserRepositoryImpl.java
@@ -1,19 +1,28 @@
 package com.pbkoffsite.web.repository;
 
 import java.util.Collection;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pbkoffsite.web.bean.AuthUserDetails;
 import com.pbkoffsite.web.bean.Role;
+import com.pbkoffsite.web.bean.UserForm;
 
 @Repository
 @Transactional
+@SuppressWarnings("unchecked")
 public class UserRepositoryImpl implements UserRepository {
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@PersistenceUnit
 	private EntityManagerFactory emf;
@@ -37,5 +46,49 @@ public class UserRepositoryImpl implements UserRepository {
 		
 		return user.getRoles();
 	}
+
+	@Override
+	public AuthUserDetails create(UserForm user) {
+		
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		
+		Role role = (Role)em.createQuery("FROM Role as role WHERE role.role = :userRole")
+				.setParameter("userRole", user.getRole())
+				.getSingleResult();
+		AuthUserDetails newUser = new AuthUserDetails();
+		newUser.setUsername(user.getUsername());
+		newUser.setFirstName(user.getFirstName());
+		newUser.setLastName(user.getLastName());
+		newUser.setPassword(passwordEncoder.encode(user.getPassword()));
+		newUser.addRole(role);
+		
+		em.persist(newUser);
+		em.getTransaction().commit();
+		
+		return null;
+	}
+
+	@Override
+	public AuthUserDetails update(AuthUserDetails user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public List<AuthUserDetails> list() {
+		return emf.createEntityManager()
+				.createQuery("FROM AuthUserDetails As user")
+				.getResultList();
+	}
+	
+	@Override
+	public List<Role> listRoles() {
+		return emf.createEntityManager()
+				.createQuery("FROM Role AS role")
+				.getResultList();
+	}
+	
+	
 	
 }
