00ce3a4a96be8de2c676ccee5c0fc12d4a7ddf5b
jmw <jmw5598@gmail.com>
2016-07-28 12:34:35 -0400

    added path to undo removal of items for admin roles

--- src/main/java/com/pbkoffsite/web/controller/AdminController.java
+++ src/main/java/com/pbkoffsite/web/controller/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -77,5 +78,11 @@ public class AdminController {
 		
 		return "removed";
 		
+	}
+	
+	@RequestMapping(value="/item/remove/undo/{id}", method=RequestMethod.GET)
+	public String undoRemovedItem(@PathVariable("id") int itemId) {
+		itemService.undoRemove(itemId);
+		return "redirect:/admin/item/remove";
 	}
 }
